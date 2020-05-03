a4db62aaf91824ac2f93687d8ec27bc8dfdfc675
NormanHussey <norm.hussey@gmail.com>
2020-05-02 20:46:26 -0400

    updated some session info

--- components/connect.php
+++ components/connect.php
@@ -1,11 +1,16 @@
 <?php
 
   $servername = "localhost";
+
   $username = "norm";
-  $password = "admin12345";
+  $dbname = 'mousehunter';
+  $password = "Z9V2xAt4gmZrf0pq";
+  // $username = "normanhu_admin";
+  // $dbname = 'normanhu_mousehunter';
+  // $password = "admin";
 
   try {
-    $db = new PDO("mysql:host=$servername;dbname=mouseHunter;charset=utf8mb4", $username, $password);
+    $db = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Connected successfully";
