42d3df23080606209ec78f5fdb8e0425453f8370
NormanHussey <norm.hussey@gmail.com>
2020-05-02 21:05:28 -0400

    fixed session_start and header issues

--- logout.php
+++ logout.php
@@ -2,8 +2,10 @@
 
   // $user = array('name'=>'Guest');
   session_start();
+  // require_once './components/connect.php';
+  // unset($_SESSION['user']);
   session_destroy();
-  echo 'logging out';
   header('Location: index.php');
+  // echo 'logging out';
 
 ?>
